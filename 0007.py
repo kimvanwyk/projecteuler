4e0d61d1e99f7ef27801db916de77b3c144fb554
Kim van Wyk <vanwykk@gmail.com>
2017-10-22 14:57:25 +0200

    Soln to 7 and 8

---
+++ 0007.py
@@ -0,0 +1,27 @@
+# original Eratosthenes prime sieve from http://www.macdevcenter.com/pub/a/python/excerpt/pythonckbk_chap1/index1.html?page=2
+# with additions from https://stackoverflow.com/questions/2211990/how-to-implement-an-efficient-infinite-generator-of-prime-numbers-in-python
+
+TARGET = 10001
+
+import itertools
+
+def eratosthenes():
+    d = {}
+    yield 2
+    for q in itertools.islice(itertools.count(3), 0, None, 2):
+        p = d.pop(q, None)
+        if p is None:
+            d[q*q] = q
+            yield q
+        else:
+            x = 2*p + q
+            while x in d:
+                x += 2*p
+            d[x] = p
+
+l = 0
+for p in eratosthenes():
+    l += 1
+    if l == TARGET:
+        print p
+        break
